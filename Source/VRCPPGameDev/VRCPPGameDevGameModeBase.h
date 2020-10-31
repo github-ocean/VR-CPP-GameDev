955594509d3767e66e06be08bd0fccfbd2a3a264
github-ocean <62124680+github-ocean@users.noreply.github.com>
2020-10-31 12:30:51 +0530

    First Commit _Project Start

---
+++ Source/VRCPPGameDev/VRCPPGameDevGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VRCPPGameDevGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRCPPGAMEDEV_API AVRCPPGameDevGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
